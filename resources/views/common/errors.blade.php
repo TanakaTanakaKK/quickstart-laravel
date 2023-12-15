068aeab88ffb4a588d72d06facbc43d465981304
KentaTanaka <kenta_tanaka@ibg-m.co.jp>
2023-12-15 18:56:02 +0900

    不要なコメントの削除

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -2,8 +2,6 @@
 
 @if(count($errors)>0)
     {{-- Form Error List --}}
-    {{-- smallを追加 タブレット以下では文字列が小さくなる --}}
-    {{-- pb-0を追加 アラート部分がデカかった為 --}}
     <div class="alert alert-danger small pb-0">
         <strong>おや？　何かがおかしいようです！</strong>
         
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,23 +11,18 @@
 
 </head>
 <body>
-    {{-- navbar-defaultからnavbar-lightに 文字を黒に--}}
-    {{-- navbar-expand-lgを追加 狭い画面の際折りたたまれて表示--}}
-    {{-- bg-lightを追加 バーの背景色が灰色に--}}
-    {{-- borderを追加 ナビゲーションバーに枠線が追加 --}}
+
     <nav class="navbar navbar-expand-lg navbar-light bg-light border text-muted">
         <div class="container">
             {{-- navbar --}}
             <div class="navbar-header">
-                {{-- p-0を追加 ナビゲーションバーの高さを調整 --}}
-                {{-- text-mutedを追加 文字列を灰色に --}}
                 <a class="navbar-brand p-0 text-muted" href="{{ url('/') }}">
                 Task List
                 </a>
             </div>
         </div>
     </nav>
-    {{--　子が自由に書ける場所 --}}
+
     @yield('content')
     {{-- JavaScripts --}}
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
