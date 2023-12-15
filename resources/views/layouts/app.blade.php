df12155a19a387c0d39a5b0fb34f9aa45e7777c8
KentaTanaka <kenta_tanaka@ibg-m.co.jp>
2023-12-15 18:49:56 +0900

    不要なlinkタグの削除

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,6 @@
     type="text/css">
     {{-- CSS bootstrap4 --}}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/resources/css/app.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 </head>
