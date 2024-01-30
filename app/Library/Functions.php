2dad998d9edc14508d44eb4ff975994124c57583
KentaTanaka <kenta_tanaka@ibg-m.co.jp>
2024-01-30 10:42:52 +0900

    ユーザーのIPを取得する関数の作成

---
+++ app/Library/Functions.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Library;
+
+use Illuminate\Support\Facades\Facade;
+
+class Functions extends Facade
+{
+    public static function get_request_ip() {
+        dd($_SERVER['HTTP_X_FORWARDED_FOR']);
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ipArray = array_map('trim', explode(',', $_SERVER["HTTP_X_FORWARDED_FOR"]));
+            $ip = $ipArray[0];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $ip;
+    }
+}
