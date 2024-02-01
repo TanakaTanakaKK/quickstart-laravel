34ce27cd4494cc0f7e0953a00c15ba4ead6a21a4
KentaTanaka <kenta_tanaka@ibg-m.co.jp>
2024-02-01 16:34:50 +0900

    不要箇所の削除

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
     AuthenticationController,
     LoginCredentialController,
-    ResetEmailController,
-    PasswordResetAuthenticationController,
     TaskController,
     UserController
 };
