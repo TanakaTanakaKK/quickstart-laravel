140561ded04e8cf3f40f0d4d901ab4661003888a
KentaTanaka <kenta_tanaka@ibg-m.co.jp>
2024-02-07 17:39:15 +0900

    スケジュールの登録

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('app:chat-work-alert')
+        ->dailyAt('17:00');
     }
 
     /**
